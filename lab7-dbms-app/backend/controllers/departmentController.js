6cf745ab31f0998264ce9e1ab5327b777e1a0e71
Ikhlas <ikhlasakhan372@gmail.com>
2024-11-02 01:00:07 +0500

    all files from lab 7 updated

---
+++ lab7-dbms-app/backend/controllers/departmentController.js
@@ -0,0 +1,58 @@
+const {
+  listAllDepartments,
+  newDepartment,
+  modifyDepartment,
+  departmentByID,
+  getMaxID,
+} = require("../models/DepartmentModel");
+const db = require("../config/db");
+
+async function getAllDepartments(req, res) {
+//LAB TASK: Finish implementation
+
+  try {
+    const employees = await listAllDepartments();
+    res.json({ data: employees });
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching departments", error: err });
+   }
+}
+
+async function getIDMax(req, res){
+  try {
+    const department = await getMaxID();
+    res.json({data:department});
+  } catch (err) {
+    res.status(500).json({message: "Error fetching the max ID", error: err})
+  } 
+}
+
+async function getDepartmentByID(req, res) {
+  try {
+    const id = req.params.id;
+    const employee = await departmentByID(id);
+    res.json({ data: employee });
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching departments", error: err });
+  }
+}
+
+async function addDepartment(req, res) {
+  try {
+    await newDepartment(req.body);
+    res.status(201).json({ message: "Department added successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error adding department", error: err });
+  }
+}
+
+async function updateDepartment(req, res) {
+  try {
+    await modifyDepartment(req.body);
+    res.status(201).json({ message: "Department updated successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error updating department", error: err });
+  }
+}
+
+module.exports = { getAllDepartments, addDepartment, updateDepartment, getDepartmentByID, getIDMax };
