2798f25bf0fe504e7a8daa8bba38fc4ae95b3dc1
Mahnoor Adeel <adeel.mahnoor03@gmail.com>
2024-12-06 14:08:31 +0500

    d4if

--- lab7-dbms-app/frontend/src/pages/Homepage/Home.js
+++ lab7-dbms-app/frontend/src/pages/Homepage/Home.js
@@ -16,8 +16,8 @@ function Home() {
   return (
     <div className="home-container d-flex flex-column justify-content-between">
       <nav class="navbar navbar-expand-lg pt-3" style={{textShadow: "0 0 15px #5c5c5c"}}>
-        <div class="container-fluid">
-          <a class="navbar-brand text-light fw-bold ps-3 fs-3" href="#" style={{ letterSpacing: "2px" }}>FlyEase<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-send mx-2 fw-bold" viewBox="0 0 16 16">
+        <div class="container-fluid ps-5 pe-3">
+          <a class="navbar-brand text-light fw-bold fs-3 ms-3" href="#" style={{ letterSpacing: "2px" }}>FlyEase<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-send mx-2 fw-bold" viewBox="0 0 16 16">
             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
           </svg></a>
 
@@ -34,6 +34,7 @@ function Home() {
             </a>
 
             </li>
+            <li class="nav-item"><a href="#" class="nav-link fw-semibold text-light fs-5 px-3"><button className="btn btn-success fw-semibold" onClick={handleSignIn}>Sign In</button></a></li>
 
           </ul>
         </div>
